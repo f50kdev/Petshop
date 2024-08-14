113e546ebbf548bedce10d0a13bea0901d52e398
f50kdev <eadpea2020@gmail.com>
2024-08-15 00:06:14 +0530

    last update

---
+++ seet.dart
@@ -0,0 +1,159 @@
+Container(
+    width: 250,
+    height: 113.72,
+    child: Stack(
+        children: [
+            Positioned(
+                left: 0,
+                top: 0,
+                child: Container(
+                    width: 250,
+                    height: 113.72,
+                    decoration: ShapeDecoration(
+                        color: Colors.white.withOpacity(0.029999999329447746),
+                        shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                                width: 1,
+                                strokeAlign: BorderSide.strokeAlignOutside,
+                                color: Colors.white.withOpacity(0.07000000029802322),
+                            ),
+                            borderRadius: BorderRadius.circular(5),
+                        ),
+                    ),
+                ),
+            ),
+            Positioned(
+                left: 20,
+                top: 20,
+                child: Text(
+                    'Your Pages and profiles',
+                    style: TextStyle(
+                        color: Colors.white.withOpacity(0.699999988079071),
+                        fontSize: 14,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w600,
+                        height: 0,
+                        letterSpacing: 0.28,
+                    ),
+                ),
+            ),
+               for({
+               })
+            Positioned(
+                left: 190,
+                top: 53,
+                child: Container(
+                    width: 40,
+                    height: 40,
+                    child: Stack(
+                        children: [
+                            Positioned(
+                                left: 0,
+                                top: 0,
+                                child: Container(
+                                    width: 40,
+                                    height: 40,
+                                    decoration: ShapeDecoration(
+                                        color: Colors.white.withOpacity(0.10000000149011612),
+                                        shape: OvalBorder(),
+                                    ),
+                                ),
+                            ),
+                        ],
+                    ),
+                ),
+            ),
+            Positioned(
+                left: 20,
+                top: 53,
+                child: Container(
+                    width: 137,
+                    height: 40.72,
+                    child: Stack(
+                        children: [
+                            Positioned(
+                                left: 48,
+                                top: 2.50,
+                                child: Container(
+                                    width: 89,
+                                    height: 35,
+                                    child: Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                            Text(
+                                                'Bitcode',
+                                                style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 14,
+                                                    fontFamily: 'Poppins',
+                                                    fontWeight: FontWeight.w600,
+                                                    height: 0,
+                                                    letterSpacing: 0.42,
+                                                ),
+                                            ),
+                                            const SizedBox(height: 2),
+                                            Text(
+                                                '@faustino_Henriques',
+                                                style: TextStyle(
+                                                    color: Color(0x99868181),
+                                                    fontSize: 8,
+                                                    fontFamily: 'Poppins',
+                                                    fontWeight: FontWeight.w500,
+                                                    height: 0,
+                                                ),
+                                            ),
+                                        ],
+                                    ),
+                                ),
+                            ),
+                            Positioned(
+                                left: 0,
+                                top: 0,
+                                child: Container(
+                                    width: 40,
+                                    height: 40,
+                                    decoration: ShapeDecoration(
+                                        image: DecorationImage(
+                                            image: NetworkImage("https://via.placeholder.com/40x40"),
+                                            fit: BoxFit.cover,
+                                        ),
+                                        shape: RoundedRectangleBorder(
+                                            side: BorderSide(width: 1, color: Color(0xFFC3C3C3)),
+                                            borderRadius: BorderRadius.circular(819),
+                                        ),
+                                    ),
+                                ),
+                            ),
+                            Positioned(
+                                left: 25.24,
+                                top: 26.96,
+                                child: Container(
+                                    width: 13.75,
+                                    height: 13.75,
+                                    child: Stack(
+                                        children: [
+                                            Positioned(
+                                                left: 0,
+                                                top: 0,
+                                                child: Container(
+                                                    width: 13.75,
+                                                    height: 13.75,
+                                                    decoration: ShapeDecoration(
+                                                        color: Color(0xFFDF0000),
+                                                        shape: OvalBorder(),
+                                                    ),
+                                                ),
+                                            ),
+                                        ],
+                                    ),
+                                ),
+                            ),
+                        ],
+                    ),
+                ),
+            ),
+        ],
+    ),
+)
